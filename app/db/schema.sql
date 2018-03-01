0a86db36b80518b4572e1a9c90493632ac1ffd6b
hmalle <hmalle90ak@gmail.com>
2018-03-01 14:25:43 -0800

    added files

---
+++ app/db/schema.sql
@@ -0,0 +1,14 @@
+
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured TINYINT(1),
+  date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
