22e54ab9a9674a550821e178271ad4deaf8369ff
hmalle <hmalle90ak@gmail.com>
2018-03-02 23:34:28 -0800

    added jaws db for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 
 var mysql=require("mysql");
+var connection;
 
+/*
 var connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
@@ -14,6 +16,20 @@ connection.connect(function(error){
   }
   console.log("MYSQL: connection id : " + connection.threadId);
 });
+*/
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection =mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"burgers_db"
+  });
+};
+
+connection.connect();
 
 module.exports = connection;
 
